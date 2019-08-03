5216c208d81d635bfa0ffabcc5569747d5c3d130
jansseneda-adobe <danimaejanssen@gmail.com>
2019-08-03 02:10:01 -0500

    version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 test_deps = ['mock', 'pytest', 'pytest-cov']
 
 setup(name='oneroster',
-      version='0.0.5',
+      version='0.0.6',
       description='Simple Oneroster client for user management',
       long_description=long_description,
       long_description_content_type="text/markdown",
